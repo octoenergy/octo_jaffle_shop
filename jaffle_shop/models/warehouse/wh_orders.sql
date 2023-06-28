dca7f1ba72d1be80b45492a7d8e5f916a8c28b23
matthew-elsham <matthew.elsham@octoenergy.com>
2023-06-28 17:32:36 +0100

    changes for new configs

--- jaffle_shop/models/warehouse/wh_orders.sql
+++ jaffle_shop/models/warehouse/wh_orders.sql
@@ -1,56 +1,37 @@
 {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
-with orders as (
+WITH order_payments AS (
 
-    select * from {{ ref('stg_orders') }}
-
-),
-
-payments as (
-
-    select * from {{ ref('stg_payments') }}
-
-),
-
-order_payments as (
-
-    select
-        order_id,
+    SELECT
+        payments.order_id
 
         {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
+        , sum(CASE WHEN payment_method = '{{ payment_method }}' THEN payments.amount ELSE 0 END) AS {{ payment_method }}_amount
         {% endfor -%}
 
-        sum(amount) as total_amount
+        , sum(payments.amount) AS total_amount
 
-    from payments
+    FROM {{ ref('stg_payments') }} payments
 
-    group by order_id
-
-),
-
-final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        orders.status,
-
-        {% for payment_method in payment_methods -%}
-
-        order_payments.{{ payment_method }}_amount,
-
-        {% endfor -%}
-
-        order_payments.total_amount as amount_dollars
-
-    from orders
-
-
-    left join order_payments
-        on orders.order_id = order_payments.order_id
+    GROUP BY payments.order_id
 
 )
 
-select * from final
+SELECT
+    orders.order_id
+    , orders.customer_id
+    , orders.order_date
+    , orders.status
+
+    {% for payment_method in payment_methods -%}
+
+    , order_payments.{{ payment_method }}_amount
+
+    {% endfor -%}
+
+    , order_payments.total_amount AS amount_dollars
+
+FROM {{ ref('stg_orders') }} orders
+
+LEFT JOIN order_payments
+    ON orders.order_id = order_payments.order_id
