7390c876d83a0aeb2754da67d28e8aebbc821e50
gdocto <george.doucy@octoenergy.com>
2023-07-11 17:10:59 +0100

    combine sales exposures with _models and lint sales

--- jaffle_shop/models/final/sales/fnl_sales_newcustomerhistory.sql
+++ jaffle_shop/models/final/sales/fnl_sales_newcustomerhistory.sql
@@ -1,4 +1,4 @@
-select 
-    date_trunc('month', first_order) AS first_order_month
-    , count(*) AS number_customers
-from {{ ref('wh_customers') }}
+SELECT
+  DATE_TRUNC('month', first_order) AS first_order_month
+  , COUNT(DISTINCT customer_id) AS number_customers
+FROM {{ ref('wh_customers') }}
