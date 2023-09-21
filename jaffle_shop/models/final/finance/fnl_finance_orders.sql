158fcb6ae1c394aaf7aa60bf505ce84f1e8361b7
John Pilgrim <john.pilgrim@octoenergy.com>
2023-09-21 10:14:45 +0100

    Add group bys

--- jaffle_shop/models/final/finance/fnl_finance_orders.sql
+++ jaffle_shop/models/final/finance/fnl_finance_orders.sql
@@ -10,3 +10,6 @@ SELECT
     , COUNT(order_id) AS total_orders
     , status AS order_status
 FROM orders
+GROUP BY
+    customer_id
+    , order_status
--- jaffle_shop/models/final/sales/fnl_sales_newcustomers.sql
+++ jaffle_shop/models/final/sales/fnl_sales_newcustomers.sql
@@ -8,3 +8,4 @@ SELECT
     TRUNC(first_order, "MM") AS first_order_month
     , COUNT(DISTINCT customer_id) AS new_customer_count
 FROM first_orders
+GROUP BY first_order_month
