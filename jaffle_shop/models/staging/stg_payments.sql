6625ed45f802f3149692448f47ef76be70459c0a
adriana-16k <adriana.kuzmanic@octoenergy.com>
2023-10-02 11:20:52 +0100

    Payments test

--- jaffle_shop/models/staging/stg_payments.sql
+++ jaffle_shop/models/staging/stg_payments.sql
@@ -1,25 +1,6 @@
-with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_payments') }}
-
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        order_id,
-        payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
-
-    from source
-
-)
-
-select * from renamed
+SELECT
+  NULLIF(id, '') AS payment_id
+  , NULLIF(order_id, '') AS order_id
+  , NULLIF(payment_method, '') AS payment_method
+  , CAST(amount AS FLOAT) AS amount_cents
+FROM {{ ref('raw_payments') }}
