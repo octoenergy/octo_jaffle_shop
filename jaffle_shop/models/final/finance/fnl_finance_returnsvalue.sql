c4c05d12d57d1ec7c435e9a899561d48f7fe6249
MajaCernja <maja.cernja@krakentechnologies.ltd>
2024-01-03 11:33:20 +0000

    Rename wh models to pass tests

--- jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
+++ jaffle_shop/models/final/finance/fnl_finance_returnsvalue.sql
@@ -2,7 +2,7 @@ WITH fnl_finance_returnsvalue AS (
     SELECT
         orders.customer_id AS customer_id
         , SUM(orders.amount) AS total_return_value
-    FROM {{ref('wh_jaffle_orders')}} as orders
+    FROM {{ref('wh_orders')}} as orders
     WHERE orders.status = 'returned'
     GROUP BY orders.customer_id
 )
