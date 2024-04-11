715d647ef553af18731faf9668eb92771fb5d0fa
Will Carpenter <65402309+WillC3@users.noreply.github.com>
2024-04-11 16:11:23 +0100

    Fix repo

--- jaffle_shop/models/warehouse/wh_customers.sql
+++ jaffle_shop/models/warehouse/wh_customers.sql
@@ -20,7 +20,6 @@ customer_orders as (
 
         select
         customer_id,
-
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
         count(order_id) as number_of_orders
