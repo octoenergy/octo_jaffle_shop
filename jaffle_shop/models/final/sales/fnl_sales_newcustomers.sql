2405e5f5541987c2324fcedf440b590de9ddd8d6
caleb-octo <caleb.buchanan@octopusenergy.nz>
2025-02-13 11:27:09 +1300

    completed listed jaffle shop fixes + added sqlfluff
    sqlfluff is only semi configured, it works but throughs a load of errors

---
+++ jaffle_shop/models/final/sales/fnl_sales_newcustomers.sql
@@ -0,0 +1,6 @@
+SELECT
+  DATE(DATE_TRUNC('MONTH', first_order)) AS order_month_year
+  , COUNT(customer_id) AS count_new_customers
+FROM {{ ref('wh_customers') }}
+WHERE first_order IS NOT NULL
+GROUP BY DATE_TRUNC('MONTH', first_order)
